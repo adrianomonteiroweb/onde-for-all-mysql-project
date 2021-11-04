f3f8dd18ae57e8d6dd762f71c797089ac37340dd
adrianomonteiroweb <adrianomonteiroweb>
2021-11-04 10:09:46 -0300

    alterações no desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,24 +1,22 @@
-DROP DATABASE IF EXISTS spotifyclone;
+CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
-CREATE DATABASE IF NOT EXISTS spotifyclone;
+USE SpotifyClone;
 
-USE spotifyclone;
-
-CREATE TABLE subscriptions (
-subscription_id INT PRIMARY KEY AUTO_INCREMENT,
-subscription VARCHAR(40) DEFAULT 'gratuito',
-price DECIMAL(3, 2)
+CREATE TABLE account_plan (
+  plan_id INT PRIMARY KEY AUTO_INCREMENT,
+  plan VARCHAR(40) DEFAULT 'gratuito',
+  price decimal(3, 2)
 );
 
-CREATE TABLE `users` (
-user_id INT PRIMARY KEY AUTO_INCREMENT,
-`name` VARCHAR(50),
-age INT,
-subscription_id INT NOT NULL,
-FOREIGN KEY (subscription_id) REFERENCES subscriptions(subscription_id)
+CREATE TABLE `user` (
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  age INT,
+  plan_id INT NOT NULL,
+  FOREIGN KEY (plan_id) REFERENCES account_plan(plan_id)
 );
 
-CREATE TABLE artists (
+CREATE TABLE artist (
   artist_id INT PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(100)
 );
@@ -26,106 +24,99 @@ CREATE TABLE artists (
 CREATE TABLE followers (
   user_id INT NOT NULL,
   artist_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
+  FOREIGN KEY (user_id) REFERENCES user(user_id),
+  FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
   CONSTRAINT PRIMARY KEY (user_id, artist_id)
 );
 
-CREATE TABLE albums (
+CREATE TABLE album (
   album_id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(100),
   artist_id INT NOT NULL,
-  FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+  FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 );
 
-CREATE TABLE songs (
-song_id INT PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(100),
-album_id INT NOT NULL,
-FOREIGN KEY (album_id) REFERENCES albums(album_id)
+CREATE TABLE song (
+  song_id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(100),
+  album_id INT NOT NULL,
+  FOREIGN KEY (album_id) REFERENCES album(album_id)
 );
 
-CREATE TABLE `histories` (
-user_id INT NOT NULL,
-song_id INT NOT NULL,
-FOREIGN KEY (user_id) REFERENCES users(user_id),
-FOREIGN KEY (song_id) REFERENCES songs(song_id),
-CONSTRAINT PRIMARY KEY (user_id, song_id)
+CREATE TABLE `history` (
+  user_id INT NOT NULL,
+  song_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user(user_id),
+  FOREIGN KEY (song_id) REFERENCES song(song_id),
+  CONSTRAINT PRIMARY KEY (user_id, song_id)
 );
 
-INSERT INTO artists (`name`)
-VALUES
-('Walter Phoenix'),
-('Peter Strong'),
-('Lance Day'),
-('Freedie Shannon');
+INSERT INTO artist (`name`)
+VALUES ('Walter Phoenix'),
+       ('Peter Strong'),
+       ('Lance Day'),
+       ('Freedie Shannon');
 
-INSERT INTO subscriptions (subscription, price)
+INSERT INTO account_plan (plan, price)
 VALUES ('gratuito', 0),
        ('familiar', 7.99),
        ('universitário', 5.99);
        
-INSERT INTO albums (title, artist_id)
-VALUES 
-('Envious', 1),
-('Exuberant', 1),
-('Hallowed Steam', 2),
-('Incandescent', 3),
-('Temporary Culture', 4);
+INSERT INTO album (title, artist_id)
+VALUES ('Envious', 1),
+       ('Exuberant', 1),
+       ('Hallowed Steam', 2),
+       ('Incandescent', 3),
+       ('Temporary Culture', 4);
        
-INSERT INTO users (`name`, age, subscription_id)
-VALUES 
-('Thati', 23, 1),
-('Cintia', 35, 2),
-('Bill', 20, 3),
-('Roger', 45, 1);
+INSERT INTO user (`name`, age, plan_id)
+VALUES ('Thati', 23, 1),
+       ('Cintia', 35, 2),
+       ('Bill', 20, 3),
+       ('Roger', 45, 1);
        
 INSERT INTO followers (user_id, artist_id)
-VALUES 
-(1, 1),
-(1, 4),
-(1, 3),
-(2, 1),
-(2, 3),
-(3, 2),
-(3, 1),
-(4, 4);
+VALUES (1, 1),
+       (1, 4),
+       (1, 3),
+       (2, 1),
+       (2, 3),
+       (3, 2),
+       (3, 1),
+       (4, 4);
        
-INSERT INTO songs (title, album_id)
-VALUES 
-('Soul For Us', 1),
-('Reflections Of Magic', 1),
-('Dance With Her Own', 1),
-('Troubles Of My Inner Fire', 2),
-('Time Fireworks', 2),
-('Magic Circus', 3),
-('Honey, So Do I', 3),
-("Sweetie, Let's Go Wild", 3),
-('She Knows', 3),
-('Fantasy For Me', 4),
-('Celebration Of More', 4),
-('Rock His Everything', 4),
-('Home Forever', 4),
-('Diamond Power', 4),
-("Honey, Let's Be Silly", 4),
-('Thang Of Thunder', 5),
-('Words Of Her Life', 5),
-('Without My Streets', 5);
+INSERT INTO song (title, album_id)
+VALUES ('Soul For Us', 1),
+       ('Reflections Of Magic', 1),
+       ('Dance With Her Own', 1),
+       ('Troubles Of My Inner Fire', 2),
+       ('Time Fireworks', 2),
+       ('Magic Circus', 3),
+       ('Honey, So Do I', 3),
+       ("Sweetie, Let's Go Wild", 3),
+       ('She Knows', 3),
+       ('Fantasy For Me', 4),
+       ('Celebration Of More', 4),
+       ('Rock His Everything', 4),
+       ('Home Forever', 4),
+       ('Diamond Power', 4),
+       ("Honey, Let's Be Silly", 4),
+       ('Thang Of Thunder', 5),
+       ('Words Of Her Life', 5),
+       ('Without My Streets', 5);
        
-INSERT INTO `histories` (user_id, song_id)
-VALUES 
-(1, 1),
-(1, 6),
-(1, 14),
-(1, 16),
-(2, 13),
-(2, 17),
-(2, 2),
-(2, 15),
-(3, 4),
-(3, 16),
-(3, 6),
-(4, 3),
-(4, 18),
-(4, 11);
-       
+INSERT INTO `history` (user_id, song_id)
+VALUES (1, 1),
+       (1, 6),
+       (1, 14),
+       (1, 16),
+       (2, 13),
+       (2, 17),
+       (2, 2),
+       (2, 15),
+       (3, 4),
+       (3, 16),
+       (3, 6),
+       (4, 3),
+       (4, 18),
+       (4, 11);
